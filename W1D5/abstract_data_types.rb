f1fbc0a7751464d5f9791b731f59aaef3b5e6cf4
jyschwrtz <jyschwrtz@gmail.com>
2017-10-05 21:25:22 -0700

    Completes Stack Class

--- W1D5/abstract_data_types.rb
+++ W1D5/abstract_data_types.rb
@@ -1,17 +1,20 @@
 class Stack
   def initialize
-
+    @ivar = []
   end
 
   def add(el)
-
+    @ivar << el
   end
 
   def remove
-
+    @ivar.pop
   end
 
   def show
-    
+    @ivar
   end
+end
+
+
 end
